dc72a5643c544d5c5c3db64cc34699873743d6c2
thiag0-Zz <thiagof89.tf@gmail.com>
2020-10-21 15:02:44 -0300

    aula 4 finalizada

--- public/scripts/page-orphanages.js
+++ public/scripts/page-orphanages.js
@@ -5,20 +5,37 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png')
 
 // criar icone personalizado
 const icon = L.icon({
-    iconUrl: './public/images/map-marker.svg',
+    iconUrl: '/images/map-marker.svg',
     iconSize: [58, 68],
     iconAnchor: [29,68],
     popupAnchor: [170,2]
 })
 
-// criar popup
-const popup = L.popup({
-    closeButton: false,
-    className: 'map-popup',
-    minWidth: 240,
-    minHeight: 240
-}).setContent('Lar das meninas <a href="orphanage.html?id=1" class="choose-orphanage"> <img src="./public/images/arrow-white.svg"> </a>')
+function addMarker({name, id, lat, lng}) {
 
-L.marker([-2.5108731,-44.2594127], { icon })
-.addTo(map)
-.bindPopup(popup)
+    // criar popup
+    const popup = L.popup({
+        closeButton: false,
+        className: 'map-popup',
+        minWidth: 240,
+        minHeight: 240
+    }).setContent(`${name} <a href="/orphanage?id=${id}" <img src="/images/arrow-white.svg"> </a>`)
+
+    L.marker([lat, lng], { icon })
+    .addTo(map)
+    .bindPopup(popup)
+
+}
+
+const orphanagesSpan = document.querySelectorAll('.orphanages span')
+
+orphanagesSpan.forEach( span => {
+    const orphanage = {
+        id: span.dataset.id,
+        name: span.dataset.name,
+        lat: span.dataset.lat,
+        lng: span.dataset.lng
+    }
+
+    addMarker(orphanage)
+} )
---
+++ src/server.js
@@ -0,0 +1,22 @@
+const { response } = require('express')
+// dependencias
+const express = require('express')
+const server = express()
+const path = require('path')
+const pages = require('./pages.js')
+
+// servidor
+server
+.use(express.static('public'))
+
+// configurar TemplateEngine
+.set('views', path.join(__dirname, "views"))
+.set('view engine', 'hbs')
+
+// rotas
+.get('/', pages.index)
+.get('/orphanage', pages.orphanage)
+.get('/orphanages', pages.orphanages)
+.get('/create-orphanage', pages.createOrphanage)
+
+server.listen(5500)
